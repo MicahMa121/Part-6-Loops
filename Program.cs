a079a5f8e3e73d8f4989ae1f560310e82f9b5ae8
Micah Ma <701004018@GoTVDSB.ca>
2024-09-09 09:40:24 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,47 @@
+﻿namespace Part_6_Loops
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int option;
+            do
+            {
+                Console.WriteLine("Hello, please enter the program you would like to use!");
+                Console.WriteLine("1.Prompter 2.Simple Banking Machine 3.Double Roller ");
+            }
+            while (int.TryParse(Console.ReadLine(), out option));
+            switch (option)
+            {
+                case 1:
+                    Prompter();
+                    break;
+            }
+        }
+        public static void Prompter()
+        {
+            int min, max;
+            Console.WriteLine("Please enter a minimum!(integer)");
+            if (!int.TryParse(Console.ReadLine(), out min))
+            {
+                Console.WriteLine("Input Error");
+                Console.ReadLine();
+                return;
+            }
+            Console.WriteLine("Please enter a maximum!(integer)");
+            if (!int.TryParse(Console.ReadLine(), out max)||min >= max)
+            {
+                Console.WriteLine("Input Error");
+                Console.ReadLine();
+                return;
+            }
+            int input;
+            do
+            {
+                Console.WriteLine("Please enter a number between "+min +","+max);
+
+            }
+            while(int.TryParse(Console.ReadLine(),
+        }
+    }
+}
